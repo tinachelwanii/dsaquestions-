0360f0321d9a7ee27a555cbe1a9a4289fa33e6eb
tina <102860264+tinachelwanii@users.noreply.github.com>
2024-08-17 11:45:38 +0530

    Create Maximum Subarray.java

---
+++ Maximum Subarray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+        int total = 0;
+
+        for (int n : nums) {
+            if (total < 0) {
+                total = 0;
+            }
+
+            total += n;
+            res = Math.max(res, total);
+        }
+
+        return res;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int result = sol.maxSubArray(nums);
+        System.out.println("The maximum sum of a contiguous subarray is " + result);
+    }
+}
